741a9b73d48054f8891b4638ba580cbe57b4e5b1
KaliKoder <kalikoder@scircle.app>
2023-11-07 11:48:17 -0500

    updating getElementById

--- src/main/java/com/myapp/caac/controller/FrontEndController.java
+++ src/main/java/com/myapp/caac/controller/FrontEndController.java
@@ -5,8 +5,6 @@ import com.myapp.caac.entity.ExportConfigurations;
 //import com.myapp.caac.service.ConfigurationServiceOld;
 import com.myapp.caac.response.ConfigurationService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,15 +31,21 @@ public class FrontEndController {
         return "home";
     }
 
-    @GetMapping("/export")
-    public String export(Model model) throws Exception {
-        String currentPage = "export";
+    @GetMapping("/manage")
+    public String manage(Model model) throws Exception {
+        String currentPage = "manage";
         model.addAttribute("currentPage", currentPage);
         model.addAttribute("apiList", getCustomApis());
         String allApis = getCustomApis().stream().map(CustomApi::getId).collect(Collectors.joining(","));
         model.addAttribute("allApis", allApis);
         List<ExportConfigurations> exportOptions = configurationService.loadExportConfigurations();
         model.addAttribute("exportOptions", exportOptions);
+        return currentPage;
+    }
+
+    @GetMapping("/export")
+    public String export(Model model) throws Exception {
+        String currentPage = "export";
         return currentPage;
     }
 
@@ -62,6 +66,5 @@ public class FrontEndController {
         model.addAttribute("apiList", customApis);
         return "export-configuration";
     }
-
 
 }
